291a1a64309b9afc21fbececc930b227853c75ac
MiguelSperle <miguelsperle205@yahoo.com>
2023-08-11 18:24:34 -0300

    UPDATED

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -2,7 +2,5 @@ import axios from 'axios'
 
 export const api = axios.create({
   baseURL: 'http://192.168.0.13:3333',
-  // headers: {
-  //   'X-Request-Origin': 'mobile', // passando o conteúdo do (X-Request-Origin) = mobile, para a verificação lá no backend
-  // },
 })
+// esse IP generico acha o localhost
